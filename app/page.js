0babd678ddcc5855a603b8239363d643896e90ec
joySutradhor <joysutradhorcmt@gmail.com>
2025-12-02 02:49:00 +0600

    loading added

--- app/page.js
+++ app/page.js
@@ -1,35 +1,35 @@
 "use client";
 
-
-// import { useToken } from '@/components/hooks/GetToken';
-// import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import "./root.css"
-
+import { useRouter } from "next/navigation";
+import "./root.css";
+import useAuthToken from "./dashboard/Hooks/useAuthToken";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  // const token = useToken();
+  const token = useAuthToken();
   const router = useRouter();
 
-  // useEffect(() => {
-  //   if (token) {
-  //     console.log("have token");
-  //     router.push("/dashboard")
-  //   } else {
-  //     console.log("no token");
-  //     router.push("/login")
-  //   }
-  // }, [token]); 
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    if (token === null || token === undefined) return;
 
+    setLoading(false);
+
+    if (token) {
+      router.push("/dashboard");
+      console.log("token exists");
+    } else {
+      console.log("token not");
+      router.push("/login");
+    }
+  }, [token]);
 
   return (
-    <section>
-      {/* <Login /> */}
-      <h1 className='text-6xl font-bold  flex justify-center items-center h-[100vh]'>
-  
-         </h1>
+    <section className="h-screen flex justify-center items-center bg-white">
+      {loading && (
+        <p className="text-gray-600 text-sm font-semibold">Loading dashboard ......</p>
+      )}
     </section>
   );
 }
-
